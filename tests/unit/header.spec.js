03de5b2febdfe57ac6a4726c961357ab116e4e5d
Ravi <c-ravi@its-dost.com>
2020-03-07 03:03:03 +0530

    added test scenario

--- tests/unit/header.spec.js
+++ tests/unit/header.spec.js
@@ -2,8 +2,14 @@ import { shallowMount } from "@vue/test-utils";
 import Header from "@/components/Header.vue";
 
 describe("Header.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Header);
+  });
   it("initialize header component", () => {
-    const wrapper = shallowMount(Header);
     expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  it("contains img tag", () => {
+    expect(wrapper.contains("img")).toBe(true);
   });
 });
