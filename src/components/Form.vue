2953f05d6d86f0c2fd87b160ee153039f7c00e68
Ravi <c-ravi@its-dost.com>
2020-03-08 18:07:26 +0530

    added form validations

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,12 @@
 <template>
-  <form @submit.prevent="submitCustomerForm()" novalidate>
+  <form @click.prevent="submitCustomerForm()" novalidate>
     <div class="form__group" v-for="item in data.formData" :key="item.id">
-      <div class="pb-8">
-        <p class="pb-8 caption">{{ item.id }}.</p>
+      <div class="mb-8">
+        <p class="mb-8 caption">{{ item.id }}.</p>
         <label class="caption"> {{ item.label }} </label>
         <span class="caption">(required)</span>
       </div>
-      <div class="rating--container cursor pb-8">
+      <div class="rating--container cursor mb-8">
         <div
           class="caption"
           v-bind:class="[
@@ -20,34 +20,57 @@
       </div>
     </div>
     <div class="form__group">
-      <div class="pb-8">
-        <p class="pb-8 caption">3.</p>
+      <div class="mb-8">
+        <p class="mb-8 caption">3.</p>
         <label class="caption"> Your password </label>
       </div>
-      <div class="pb-8">
+      <div class="mb-8">
         <input
           type="password"
           v-model.trim="$v.customerForm.password.$model"
           class="form__control"
+          :class="{
+            'form__control--error': $v.customerForm.password.$error
+          }"
         />
+        <div v-if="errors">
+          <p
+            class="caption error mt-8"
+            v-if="!$v.customerForm.password.required"
+          >
+            Password is required
+          </p>
+          <p
+            class="caption error mt-8"
+            v-if="!$v.customerForm.password.validatePassword"
+          >
+            Password should contain atleast 1 small letter and 1 number
+          </p>
+        </div>
       </div>
     </div>
     <div class="form__group">
-      <div class="pb-8">
-        <p class="pb-8 caption">4.</p>
+      <div class="mb-8">
+        <p class="mb-8 caption">4.</p>
         <label class="caption"> Your email address </label>
       </div>
-      <div class="pb-8">
+      <div class="mb-8">
         <input
           type="email"
           v-model.trim="$v.customerForm.email.$model"
           class="form__control"
+          :class="{
+            'form__control--error': $v.customerForm.email.$error
+          }"
         />
+        <p class="caption error mt-8" v-if="$v.customerForm.email.$error">
+          Email format is not correct
+        </p>
       </div>
     </div>
     <div class="form__group text-align">
-      <div class="pb-8">
-        <input type="submit" value="Submit" class="btn btn__primary" />
+      <div class="mb-8">
+        <button type="submit" class="btn btn__primary">Submit</button>
       </div>
     </div>
   </form>
@@ -61,6 +84,7 @@ export default {
   data() {
     return {
       data: this.formData,
+      errors: false,
       customerForm: {
         password: "",
         email: ""
@@ -71,10 +95,14 @@ export default {
   validations: {
     customerForm: {
       password: {
-        required
+        required,
+        validatePassword(password1) {
+          return (
+            /[a-z]/.test(password1) && /[0-9]/.test(password1) //checks for a-z //checks for 0-9
+          );
+        }
       },
       email: {
-        required,
         email
       }
     }
@@ -84,13 +112,10 @@ export default {
       this.$v.$touch();
       if (this.$v.$invalid) {
         this.submitStatus = "ERROR";
+        this.errors = this.$v.customerForm.$anyError;
         console.log("submit error!");
       } else {
-        this.submitStatus = "PENDING";
-        setTimeout(() => {
-          this.submitStatus = "OK";
-          console.log("submit success!");
-        }, 500);
+        this.submitStatus = "OK";
       }
     }
   }
@@ -114,6 +139,12 @@ export default {
   padding: 1rem;
   border: 1px solid $border-color;
   border-radius: 0.5rem;
+  &--error {
+    border: 1px solid $error-color;
+  }
+}
+.error {
+  color: $error-color;
 }
 .age--container {
   border: 1px solid $border-color;
